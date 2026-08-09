f09515c05e24948517e01eb25c27a4e9ff3145d5
Marcus <demafelizmarcus@gmail.com>
2026-08-09 22:14:44 +1200

    edited tut5

--- tut5.py
+++ tut5.py
@@ -5,7 +5,7 @@ def solve_system3(A, k, b):
     """Solves the system A**k @ x = for 3x3 A"""
     L, U = myLU3(A)
     last_sol = b
-    for i in range(k):
+    for _ in range(k):
         last_sol = forwardSub3(L, last_sol)
         last_sol = backSub3(U, last_sol)
     return last_sol
